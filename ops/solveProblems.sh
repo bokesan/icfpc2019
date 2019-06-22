9b0922f39254ae738240a17e4e4a886dea70abe2
Chris <chbreitkopf@gmail.com>
2019-06-22 06:02:07 +0200

    Performance: keep jvm instance when solving multiple problems

--- ops/solveProblems.sh
+++ ops/solveProblems.sh
@@ -4,9 +4,4 @@ JARFILE=$1
 SOURCEDIR=$2
 TARGETDIR=$3
 
-for file in ${SOURCEDIR}/*.desc; do
-	[ -e "$file" ] || continue
-	TARGET=`echo ${file} | sed "s/.*\///" | cut -d'.' -f 1`
-	TARGET="${TARGETDIR}/${TARGET}.sol"
-	java -Xms2g -Xss500m -jar ${JARFILE} ${file} ${TARGET}
-done
+java -Xms2g -jar "$JARFILE" --target-dir "$TARGETDIR" $SOURCEDIR/*.desc

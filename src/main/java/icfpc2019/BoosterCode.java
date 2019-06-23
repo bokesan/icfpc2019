818a91faab287235f8c8188f5c053d1c072ecd24
Daniel Kauke <DAK@magrathea.eu>
2019-06-24 00:32:35 +0200

    enabled buyout file

--- src/main/java/icfpc2019/BoosterCode.java
+++ src/main/java/icfpc2019/BoosterCode.java
@@ -2,6 +2,18 @@ package icfpc2019;
 
 public enum BoosterCode {
 
-    B, F, L, X, R, C
+    B(1000),
+    F(300),
+    L(700),
+    X(0),
+    R(1200),
+    C(2000);
 
+    private int numVal;
+    BoosterCode(int numVal){
+        this.numVal = numVal;
+    }
+    public int getNumVal(){
+        return numVal;
+    }
 }

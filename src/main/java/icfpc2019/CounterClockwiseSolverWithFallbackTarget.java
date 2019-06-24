ccd64b4730ee928174c0a6d384f2aca49149a658
Christoph Breitkopf <chbreitkopf@gmail.com>
2019-06-24 03:49:57 +0200

    Merge from main.

--- src/main/java/icfpc2019/CounterClockwiseSolverWithFallbackTarget.java
+++ src/main/java/icfpc2019/CounterClockwiseSolverWithFallbackTarget.java
@@ -170,14 +170,10 @@ public class CounterClockwiseSolverWithFallbackTarget implements Solver {
     }
 
     private boolean hasFreeNeighbours(Point point) {
-        boolean trapped = true;
-        for (Point p : point.adjacent()) {
-            if (state.needsWrapping(p)) {
-                trapped = false;
-                break;
-            }
-        }
-        return !trapped;
+        return state.needsWrapping(point.up()) ||
+                state.needsWrapping(point.down()) ||
+                state.needsWrapping(point.left()) ||
+                state.needsWrapping(point.right());
     }
 
     private Point getMyFront(Robot robot) {
